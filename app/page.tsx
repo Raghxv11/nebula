68d8c81f8b90731311d09e12bcd06b932b6b9772
Raghxv11 <raghavbansal121@gmail.com>
2025-11-02 20:50:27 -0700

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ import { Footer } from "@/components/Footer"
 export default function Home() {
   return (
     <>
-      <div className="relative w-full">
+      <div className="relative w-full overflow-hidden">
         {/* background image (place the provided image at public/nebula-bg.jpg) */}
         {/* <img aria-hidden src="/nebula-bg.jpg" alt="nebula background" className="pointer-events-none absolute inset-0 w-full h-full object-cover opacity-30" /> */}
 
